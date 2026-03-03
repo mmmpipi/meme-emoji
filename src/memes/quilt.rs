7dd0041f09023f572b5958447fc4497ec8ceb730
mmmpipi <153883613+mmmpipi@users.noreply.github.com>
2026-03-03 12:43:34 +0800

    被子更新

--- src/memes/quilt.rs
+++ src/memes/quilt.rs
@@ -1,9 +1,9 @@
-use skia_safe::Color;
+use skia_safe::Image;
 
 use meme_generator_core::error::Error;
 use meme_generator_utils::{
     builder::InputImage,
-    encoder::GifEncoder,
+    encoder::{FrameAlign, GifInfo, make_gif_or_combined_gif},
     image::ImageExt,
     tools::{load_image, local_date, new_surface},
 };
@@ -12,33 +12,49 @@ use crate::{options::NoOptions, register_meme};
 
 fn quilt(images: Vec<InputImage>, _: Vec<String>, _: NoOptions) -> Result<Vec<u8>, Error> {
     let positions = [
-        (68, 68), (68, 68), (68, 72), (69, 74), (69, 74),
-        (69, 73), (69, 72), (69, 74), (68, 72), (68, 68),
-        (68, 68), (68, 68), (68, 72), (68, 72), (69, 73),
-        (69, 73), (69, 73), (69, 75), (68, 72), (68, 68),
+        (68, 68),
+        (68, 68),
+        (68, 72),
+        (69, 74),
+        (69, 74),
+        (69, 73),
+        (69, 72),
+        (69, 74),
+        (68, 72),
+        (68, 68),
+        (68, 68),
+        (68, 68),
+        (68, 72),
+        (68, 72),
+        (69, 73),
+        (69, 73),
+        (69, 73),
+        (69, 75),
+        (68, 72),
+        (68, 68),
         (68, 68),
     ];
-    
+
     let user_head = images[0].image.resize_exact((142, 146));
 
-    let mut encoder = GifEncoder::new();
-    
-    for i in 0..21 {
+    let func = |i: usize, _images: Vec<Image>| {
         let frame = load_image(format!("quilt/{}.png", i + 1))?;
-        
+        let (x, y) = positions[i];
         let mut surface = new_surface(frame.dimensions());
         let canvas = surface.canvas();
-        
-        canvas.clear(Color::TRANSPARENT);
-        
-        let (x, y) = positions[i];
         canvas.draw_image(&user_head, (x, y), None);
         canvas.draw_image(&frame, (0, 0), None);
-        
-        encoder.add_frame(surface.image_snapshot(), 0.03)?;
-    }
-    
-    Ok(encoder.finish()?)
+        Ok(surface.image_snapshot())
+    };
+    make_gif_or_combined_gif(
+        images,
+        func,
+        GifInfo {
+            frame_num: 21,
+            duration: 0.03,
+        },
+        FrameAlign::ExtendLoop,
+    )
 }
 
 register_meme! {
